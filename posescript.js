4259e92118803abe83038731153165a18afc4d0b
serusan <57762231+serusan@users.noreply.github.com>
2021-01-11 12:00:01 +0100

    Update posescript.js

--- posescript.js
+++ posescript.js
@@ -18,7 +18,7 @@ function onResults(results) {
   
   var kekw = results.poseLandmarks[0]['y'];
   if (prevkekw != 0 ) {
-	if (Math.abs(kekw - centery) > 0.001){
+	if (Math.abs(kekw - centery) > 0.00001){
 			console.log("Spring");
 			jump()
 			
